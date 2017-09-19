0d1a1cb2fe446fdc631ceaf08c86929e4f0f1021
Joel Copi <j.copi@outlook.com>
2017-09-19 19:55:12 -0400

    Finished inital menu shapes

--- index.js
+++ index.js
@@ -1,12 +1,21 @@
 _(".button").on("click", function () {
-    _(this).toggleClass("selec");
-    switch (this.id) {
-    case "lm_btn":
-        _("#leftmenu").toggleClass("open");
-        break;
-    case "nf_btn":
-        break;
-    case "ac_btn":
-        break;
+    if(_(this).hasClass("selec")) {
+        _(".button.selec").dropClass("selec");
+        _(".rightmenu, #leftmenu").dropClass("open");
+    } else {
+        _(".button.selec").dropClass("selec");
+        _(".rightmenu, #leftmenu").dropClass("open");
+        _(this).addClass("selec");
+        switch (this.id) {
+        case "lm_btn":
+            _("#leftmenu").addClass("open");
+            break;
+        case "nf_btn":
+            _("#notifmenu").addClass("open");
+            break;
+        case "ac_btn":
+            _("#accountmenu").addClass("open");
+            break;
+        }
     }
 });
